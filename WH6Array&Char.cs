f68c6749ec6caa8c0c84f4e65e6422ce37b2703c
Zewels <tzaznoba@gmail.com>
2023-12-17 16:38:45 +0300

    Add files via upload

---
+++ WH6Array&Char.cs
@@ -0,0 +1,75 @@
+﻿// Задание 1
+// Задайте двумерный массив символов (тип char [,]).
+// Создать строку из символов этого массива.
+
+string GetStringFromCharArray (char [,] chars)
+{
+    string res = "";
+    foreach (char elem in chars)
+    {
+        res += elem;
+    }
+    return res;
+}
+
+char [,] chars = {{'a','b','c','d'},
+                  {'f','g','t','y'}};
+
+string str = GetStringFromCharArray(chars);
+Console.WriteLine(str);
+
+// Задание 2
+// Задайте строку, содержащую латинские буквы в обоих регистрах.
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string FilterSring(string str)
+{
+    string res = str.ToLower();
+ 
+    return res;
+}
+
+string str = "If You Read This, That I Did It";
+Console.WriteLine(FilterSring(str));
+
+// Задание 3
+// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string FilterSring(string str)
+{
+    string res = str.ToLower();
+    string count = "";
+    foreach(char elem in res)
+    {
+        if(elem != ' ')
+        {
+            count+=elem;
+        }
+    }
+    return count;
+}
+
+string IsPalindrome(string str)
+{
+    bool flag = false;
+    for(int i = 0; i<str.Length/2; i++)
+    {
+        if(str[i] == str[str.Length-i-1])
+        {
+            flag = true;
+        }
+        else
+        {
+            return("String is not palindrome");
+        }
+    }
+    if(flag)
+    {
+        return("String is palindrome");
+    }
+    return("String is not palindrome");
+}
+
+string str = "Sagas";
+string res = FilterSring(str);
+Console.WriteLine(IsPalindrome(res));
